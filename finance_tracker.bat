abf48b3b8b858728772eb38ed612c2247e42e6d8
dan-alencar <danalencar2001@gmail.com>
2026-01-27 19:39:12 -0300

    Setup

--- finance_tracker.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-REM Abre o terminal no diretório correto e executa o app
-
-cd /d "%~dp0"
-python -m finance_app.main
-pause
--- finance_tracker.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "🚀 Tracker launching at $(date)" >> "$HOME/tracker_debug.log"
-cd "$(dirname "$0")"
-python3 -u -m finance_app.main
-
